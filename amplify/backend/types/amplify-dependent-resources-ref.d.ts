988261b5e1400ef4bc00da5b2dde882daec6183e
MRNSJ26 <jijjinoor@gmail.com>
2024-05-27 01:24:07 +0300

    amplify

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,15 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "auiseagle879b1211879b1211": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "HostedUIDomain": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "OAuthMetadata": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
